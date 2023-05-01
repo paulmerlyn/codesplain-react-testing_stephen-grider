42a6b70d9541a28e25118575fb7968eba8b67d0d
Paul Merlyn <paul@paulmerlyn.com>
2023-05-01 11:46:39 -0700

    Additional tests for the RepositoriesSummary.test.js file

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -33,10 +33,29 @@ test("Page doesn't show 'JavaScript' when repository doesn't include a language 
   render(<RepositoriesSummary repository={repository} />);
 
   // Find JSX element or manipulate the component
-  // eslint-disable-next-line testing-library/no-debugging-utils
-  screen.logTestingPlaygroundURL()
   const languageTextElementPresent = screen.queryByText(/JavaScript/i); // use queryBy rather than getBy to verify something is NOT present b/c it returns false rather than throws an error
 
   // Verify element is in the document
   expect(languageTextElementPresent).toEqual(null);
+})
+
+test("All expected repository prop values are displayed", () => {
+  const repository = {
+    stargazers_count: 21,
+    open_issues: 22,
+    forks: 23,
+    language: "TypeScript",
+  };
+
+  render(<RepositoriesSummary repository={repository} />);
+
+  // eslint-disable-next-line testing-library/no-debugging-utils
+  screen.logTestingPlaygroundURL();
+
+  for (let [key, val] of Object.entries(repository)) {
+    console.log(`val is ${val}`)
+    const regex = new RegExp(val, "i"); // create a regex from our value because it might be broken over more than one line or otherwise might not be an exact string match for screen.getByText()
+    const elem = screen.getByText(regex);
+    expect(elem).toBeInTheDocument();
+  }
 })
